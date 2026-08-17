d247520aeb5145eb75eb1b02412a4529f1aeff66
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-17 14:04:13 -0600

    Add Context and reflection imports in ReceiveService
    Added imports for Context and reflection methods.

--- app/src/main/java/com/blithe/legacysend/ReceiveService.java
+++ app/src/main/java/com/blithe/legacysend/ReceiveService.java
@@ -4,10 +4,13 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.Context;
 import android.os.Build;
 import android.os.IBinder;
 
 import com.blithe.legacysend.ui.MainActivity;
+
+import java.lang.reflect.Method;
 
 public final class ReceiveService extends Service {
 
